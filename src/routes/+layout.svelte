f68d377beb7173bd20add1af152f542f712ddd95
oliforster <oliforster798@gmail.com>
2023-06-17 14:24:57 +0100

    added login page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+	<div class="container">
+		<a class="navbar-brand" href="/">TestCms</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNav"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="/login">Login</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="container">
+	<slot />
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	let login = true;
+
+	let showLogin = () => {
+		login = !login;
+	};
+</script>
+
+{#if login}
+	<div class="card mt-4">
+		<h5 class="card-header">Login</h5>
+		<div class="card-body">
+			<form>
+				<div class="mb-3">
+					<label for="userName" class="form-label">Username</label>
+					<input type="email" class="form-control" id="userName" />
+				</div>
+				<div class="mb-3">
+					<label for="password" class="form-label">Password</label>
+					<input type="password" class="form-control" id="password" />
+				</div>
+			</form>
+			<button type="button" class="btn btn-primary">Log in</button>
+			<button type="button" class="btn btn-warning" on:click={showLogin}
+				>Go to create account</button
+			>
+		</div>
+	</div>
+{:else}
+	<div class="card mt-4">
+		<h5 class="card-header">Create account</h5>
+		<div class="card-body">
+			<form>
+				<div class="mb-3">
+					<label for="userName" class="form-label">Username</label>
+					<input type="email" class="form-control" id="userName" />
+				</div>
+				<div class="mb-3">
+					<label for="password" class="form-label">Password</label>
+					<input type="password" class="form-control" id="password" />
+				</div>
+			</form>
+			<button type="button" class="btn btn-primary">Create Account</button>
+			<button type="button" class="btn btn-warning" on:click={showLogin}>Go to login</button>
+		</div>
+	</div>
+{/if}
